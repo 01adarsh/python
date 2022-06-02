2364c1d5285f1c6329af1a3c032d29351cf32047
ADARSH <adarsh@bjshomedelivery.com>
2022-06-03 00:25:03 +0530

    first commit

---
+++ list_1.py
@@ -0,0 +1,12 @@
+#list is the most versatile datatype avilable in python ,it is mutable
+
+#creating a list
+
+list1 = ["aman", "rakshit", "pythonista", 1997, 1995]
+#indexing will starts from 0
+print(list1)
+
+#for accessing the element of list
+
+print(list1[0])
+#print aman
